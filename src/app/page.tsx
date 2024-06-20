c38712d81a321667e97934bd97e7b8a5fd294bd5
Gabriel Silveira <ramos97033@outlook.com>
2024-06-20 14:18:07 -0300

    loading icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import {useEffect, useState } from "react";
 import "./globals.css";
 import axios from "axios";
 import Title from "@/components/Titulo";
+import { AiOutlineLoading } from "react-icons/ai";
 
 
 export default function Home() {  
@@ -34,22 +35,25 @@ export default function Home() {
 
 
   return (
-    <main className="mx-16">
-      <form onSubmit={publicarPost} className="flex flex-col justify-start items-start ">
-        <Title label="Título" value={titulo} onChange={(string : any) => setTitulo(string.target.value)}/>
-        <Post label="Post" value={post} onChange={(string: any) => setPost(string.target.value)}/>
-        <button  className="relative inline-flex items-center justify-center p-3 mt-8 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">Publicar</button>
-      </form>
-      <section>
-        {loading ? posts.map((post : any) => {
-          return(
-            <section key={post.id}>
-              <h1 className="font-bold text-lg">{post.titulo}</h1>
-              <p>{post.post}</p>
-            </section>
-          );
-        })  :  <p className="flex justify-center">Loading</p>}
-      </section>
-    </main>
+    <>
+      <h1 className=" font-bold text-xl flex justify-center text-center pt-10">Nossa Social-Média</h1>
+      <main className="mx-16 flex justify-around items-center mt-36 gap-7">
+        <form onSubmit={publicarPost} className="flex flex-col justify-start items-start ">
+          <Title label="Título" value={titulo} onChange={(string : any) => setTitulo(string.target.value)}/>
+          <Post label="Post" value={post} onChange={(string: any) => setPost(string.target.value)}/>
+          <button className="relative inline-flex items-center justify-center p-3 mt-6 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">Publicar</button>
+        </form>
+        <section>
+          {loading ? posts.map((post : any) => {
+            return(
+              <section key={post.id}>
+                <h1 className="font-bold text-lg">{post.titulo}</h1>
+                <p>{post.post}</p>
+              </section>
+            );
+          })  :  <AiOutlineLoading className="animate-spin text-5xl" />}
+        </section>
+      </main>
+    </>
   );
 }
